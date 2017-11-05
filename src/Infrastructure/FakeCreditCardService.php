c023074495a3f58deb3121dab7d2b9f8092d5038
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-11-05 13:49:45 +0100

    Move FakeCreditCardService out of todo/
    It was misplaced there since nothing in todo/ used it

---
+++ src/Infrastructure/FakeCreditCardService.php
@@ -0,0 +1,17 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\Fundraising\Frontend\PaymentContext\Infrastructure;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Kai Nissen < kai.nissen@wikimedia.de >
+ */
+class FakeCreditCardService implements CreditCardService {
+
+	public function getExpirationDate( string $customerId ): CreditCardExpiry {
+		return new CreditCardExpiry( 9, 2038 );
+	}
+
+}
