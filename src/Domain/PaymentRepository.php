f92c850f63f7e9422a5280d0b6977bebf0859e6d
Gabriel Birke <gabriel.birke@posteo.net>
2022-02-28 17:32:30 +0100

    Prevent payment override
    This commits defines the "duplicate" insertion of payments.
    
    This means payments must always be modified (e.g. booked) in-place
    instead of creating an new payment with the same id.

--- src/Domain/PaymentRepository.php
+++ src/Domain/PaymentRepository.php
@@ -3,10 +3,22 @@ declare( strict_types=1 );
 
 namespace WMDE\Fundraising\PaymentContext\Domain;
 
+use WMDE\Fundraising\PaymentContext\DataAccess\PaymentNotFoundException;
+use WMDE\Fundraising\PaymentContext\DataAccess\PaymentOverrideException;
 use WMDE\Fundraising\PaymentContext\Domain\Model\Payment;
 
 interface PaymentRepository {
+	/**
+	 * @param Payment $payment
+	 * @return void
+	 * @throws PaymentOverrideException
+	 */
 	public function storePayment( Payment $payment ): void;
 
+	/**
+	 * @param int $id
+	 * @return Payment
+	 * @throws PaymentNotFoundException
+	 */
 	public function getPaymentById( int $id ): Payment;
 }
