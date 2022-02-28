65d1f11d840a6e67ce01322cd176a8c60d5d04b3
Gabriel Birke <gabriel.birke@posteo.net>
2022-02-28 17:38:04 +0100

    Handle unbooked legacy credit card payments
    getLegacyData should return an empty array when payment is not booked

--- src/Domain/Model/LegacyDataTransformer.php
+++ src/Domain/Model/LegacyDataTransformer.php
@@ -7,7 +7,9 @@ namespace WMDE\Fundraising\PaymentContext\Domain\Model;
 /**
  * This interface will output the legacy data for storing payment data in the "data blob" of donations/memberships.
  * As long as code is reading that legacy data (exporter, detail view of FOC), we need to preserve this interface.
- * When the full refactoring is done, remove this interface and all its implementations
+ * When the full refactoring is done, remove this interface and all its implementations.
+ *
+ * For bookable payment types (Sofort, Credit Card, PayPal), this should return an empty array when the payment is not booked.
  */
 interface LegacyDataTransformer {
 
