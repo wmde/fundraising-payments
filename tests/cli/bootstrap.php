4a704eb0bf40ffdba7c0dbfc20a1cc27d0e992b5
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-03-16 16:51:28 +0100

    Refactor SofortPayment
    - implements sofortüberweisung tests for the doctrinePaymentRepository
    - adapts SofortPayment class according to our data model
    
    https://phabricator.wikimedia.org/T301806

--- tests/cli/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare( strict_types = 1 );
-
-use WMDE\Fundraising\PaymentContext\Tests\cli\InspectorGeneratorCommand;
-
-require __DIR__ . '/../bootstrap.php';
-InspectorGeneratorCommand::run();
