49e48cebf914fa2976354b4ee269515800ac8e9e
Gabriel Birke <gabriel.birke@posteo.net>
2022-03-25 12:54:38 +0100

    Use PaymentReferenceCode in code generator
    Remove some of the test cases in LessSimpleTransferCodeGenerator because
    they are covered by PaymentReferenceCode.

--- src/Domain/LessSimpleTransferCodeGenerator.php
+++ src/Domain/LessSimpleTransferCodeGenerator.php
@@ -4,20 +4,13 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\PaymentContext\Domain;
 
-use InvalidArgumentException;
 use Iterator;
+use WMDE\Fundraising\PaymentContext\Domain\Model\PaymentReferenceCode;
 
 /**
  * @todo Rename to RandomTransferCodeGenerator, use TransferCode class
  */
 class LessSimpleTransferCodeGenerator implements TransferCodeGenerator {
-
-	public const ALLOWED_CHARACTERS = 'ACDEFKLMNPRTWXYZ349';
-	public const READABILITY_DELIMITER = '-';
-
-	public const LENGTH_PREFIX = 2;
-	public const LENGTH_CODE = 6;
-	public const LENGTH_CHECKSUM = 1;
 
 	/**
 	 * @var Iterator<string>
@@ -31,14 +24,14 @@ class LessSimpleTransferCodeGenerator implements TransferCodeGenerator {
 	private function __construct( Iterator $characterSource ) {
 		$this->characterSource = $characterSource;
 
-		$this->checksumGenerator = new ChecksumGenerator( str_split( self::ALLOWED_CHARACTERS ) );
+		$this->checksumGenerator = new ChecksumGenerator( str_split( PaymentReferenceCode::ALLOWED_CHARACTERS ) );
 	}
 
 	public static function newRandomGenerator(): static {
 		return new self(
 			( static function (): iterable {
-				$characterCount = strlen( self::ALLOWED_CHARACTERS );
-				$characters = str_split( self::ALLOWED_CHARACTERS );
+				$characterCount = strlen( PaymentReferenceCode::ALLOWED_CHARACTERS );
+				$characters = str_split( PaymentReferenceCode::ALLOWED_CHARACTERS );
 				// See https://github.com/phpstan/phpstan/issues/6189
 				// @phpstan-ignore-next-line
 				while ( true ) {
@@ -57,34 +50,22 @@ class LessSimpleTransferCodeGenerator implements TransferCodeGenerator {
 	}
 
 	public function generateTransferCode( string $prefix ): string {
-		if ( strlen( $prefix ) !== self::LENGTH_PREFIX ) {
-			throw new InvalidArgumentException(
-				sprintf(
-					'The prefix must have a set length of %d characters.',
-					self::LENGTH_PREFIX
-				)
-			);
-		}
-		if ( !preg_match( '/[' . preg_quote( self::ALLOWED_CHARACTERS ) . ']/', $prefix ) ) {
-			throw new InvalidArgumentException(
-				'The prefix must only contain characters from the ALLOWED_CHARACTERS set.'
-			);
-		}
-
-		$code = $prefix . $this->generateCode();
-		$code .= $this->checksumGenerator->createChecksum( $code );
-
-		return $this->formatCodeForReadability( $code );
+		return $this->generatePaymentReferenceCode( $prefix )->getFormattedCode();
 	}
 
-	private function formatCodeForReadability( string $code ): string {
-		return vsprintf( '%s%s-%s%s%s-%s%s%s-%s', str_split( $code ) );
+	public function generatePaymentReferenceCode( string $prefix ): PaymentReferenceCode {
+		$code = $this->generateCode();
+		return new PaymentReferenceCode(
+			$prefix,
+			$code,
+			$this->checksumGenerator->createChecksum( $prefix . $code )
+		);
 	}
 
 	private function generateCode(): string {
 		$transferCode = '';
 
-		for ( $i = 0; $i < self::LENGTH_CODE; $i++ ) {
+		for ( $i = 0; $i < PaymentReferenceCode::LENGTH_CODE; $i++ ) {
 			$transferCode .= $this->characterSource->current();
 			$this->characterSource->next();
 		}
