b85405ff9b0f112a327fabd04808306821e397f4
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-06-17 17:25:20 +0200

    Improve SofortConfig constructor
    Drop reasonText and use the previously-unused TranslatableDescription instead.
    This makes the configuration and URL generator more consistent with the
    others.

--- src/Domain/PaymentUrlGenerator/SofortConfig.php
+++ src/Domain/PaymentUrlGenerator/SofortConfig.php
@@ -6,25 +6,12 @@ namespace WMDE\Fundraising\PaymentContext\Domain\PaymentUrlGenerator;
 
 class SofortConfig {
 
-	private string $reasonText;
-	private string $locale;
-	private string $returnUrl;
-	private string $cancelUrl;
-	private string $notificationUrl;
-	private TranslatableDescription $translatableDescription;
-
-	public function __construct( string $reasonText, string $locale, string $returnUrl, string $cancelUrl, string $notificationUrl,
-		TranslatableDescription $translatableDescription ) {
-		$this->reasonText = $reasonText;
-		$this->locale = $locale;
-		$this->returnUrl = $returnUrl;
-		$this->cancelUrl = $cancelUrl;
-		$this->notificationUrl = $notificationUrl;
-		$this->translatableDescription = $translatableDescription;
-	}
-
-	public function getReasonText(): string {
-		return $this->reasonText;
+	public function __construct(
+		private string $locale,
+		private string $returnUrl,
+		private string $cancelUrl,
+		private string $notificationUrl,
+		private TranslatableDescription $translatableDescription ) {
 	}
 
 	public function getLocale(): string {
