b1b7d04475d40f136cdb65057676b3baded887f7
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-07-28 10:00:36 +0200

    Use getDoctrineMappingPaths in context factory
    This is for https://phabricator.wikimedia.org/T312080

--- src/PaymentContextFactory.php
+++ src/PaymentContextFactory.php
@@ -6,25 +6,18 @@ namespace WMDE\Fundraising\PaymentContext;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Type;
-use Doctrine\ORM\Mapping\Driver\XmlDriver;
-use Doctrine\Persistence\Mapping\Driver\MappingDriver;
 
 class PaymentContextFactory {
 
-	/**
-	 * Use this constant for MappingDriverChain::addDriver
-	 */
-	public const ENTITY_NAMESPACE = 'WMDE\Fundraising\PaymentContext\Domain\Model';
-
-	public const DOCTRINE_CLASS_MAPPING_DIRECTORY = __DIR__ . '/../config/DoctrineClassMapping';
+	private const DOCTRINE_CLASS_MAPPING_DIRECTORY = __DIR__ . '/../config/DoctrineClassMapping';
 
 	/**
-	 * @return MappingDriver
-	 * @deprecated We no longer have a mix of mapping drivers.
-	 *     Use {@see XmlDriver} with an array containing {@see PaymentContextFactory::DOCTRINE_CLASS_MAPPING_DIRECTORY}} instead.
+	 * Return filesystem paths to Doctrine mapping files (.dcm.xml files)
+	 *
+	 * @return string[]
 	 */
-	public function newMappingDriver(): MappingDriver {
-		return new XmlDriver( self::DOCTRINE_CLASS_MAPPING_DIRECTORY );
+	public function getDoctrineMappingPaths(): array {
+		return [ self::DOCTRINE_CLASS_MAPPING_DIRECTORY ];
 	}
 
 	public function registerCustomTypes( Connection $connection ): void {
