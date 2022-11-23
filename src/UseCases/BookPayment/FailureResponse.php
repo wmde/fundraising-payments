d5cb83fa83ee59a247b5e93e12a6235e2b76576e
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-11-23 10:17:56 +0100

    Add paymentWasAlreadyBooked to FailureResponse
    Add a new static constructor and a boolean getter to check if the
    FailureResponse is caused by an already booked payment. This will help
    upstream code to check against this condition without using hard-coded
    string messages (that might change).
    
    Ticket: https://phabricator.wikimedia.org/T321346

--- src/UseCases/BookPayment/FailureResponse.php
+++ src/UseCases/BookPayment/FailureResponse.php
@@ -5,8 +5,19 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\PaymentContext\UseCases\BookPayment;
 
 class FailureResponse {
+
+	private const ALREADY_COMPLETED = 'Payment is already completed';
+
 	public function __construct(
 		public readonly string $message
 	) {
+	}
+
+	public static function newAlreadyCompletedResponse(): self {
+		return new self( self::ALREADY_COMPLETED );
+	}
+
+	public function paymentWasAlreadyCompleted(): bool {
+		return $this->message === self::ALREADY_COMPLETED;
 	}
 }
