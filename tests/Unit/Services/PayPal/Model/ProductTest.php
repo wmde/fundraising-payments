6021f310d015e641bb027853670211265a1fa30a
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-05-05 18:44:51 +0200

    Add createProduct to API
    Add JSON serialization to Product
    Add createProduct method to GuzzlePaypalAPI
    
    Co-Authored-By: Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
    Co-Authored-By: Sperling-0 <tanuja.doriya@wikimedia.de>

---
+++ tests/Unit/Services/PayPal/Model/ProductTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WMDE\Fundraising\PaymentContext\Tests\Unit\Services\PayPal\Model;
+
+use PHPUnit\Framework\TestCase;
+use WMDE\Fundraising\PaymentContext\Services\PayPal\Model\Product;
+
+/**
+ * @covers \WMDE\Fundraising\PaymentContext\Services\PayPal\Model\Product
+ */
+class ProductTest extends TestCase {
+
+	public function testToJSONSerialization(): void {
+		$product = new Product( 'SerializationName', 'SerializationID', 'SerializationDescription' );
+
+		$actualJSONOutput = $product->toJSON();
+
+		$this->assertSame(
+			'{"name":"SerializationName","id":"SerializationID","description":"SerializationDescription","category":"NONPROFIT","type":"SERVICE"}',
+			$actualJSONOutput
+		);
+	}
+
+}
