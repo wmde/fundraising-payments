a8bf2270f392d60fff9b96c435f580b3a2f90754
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-06-15 18:36:22 +0200

    Expland ListSubscriptionPlansCommand code
    Read env variables, ignore `.env` file, initialize API and list products
    
    Co-Authored-By: Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
    Co-Authored-By: Sperling-0 <tanuja.doriya@wikimedia.de>

--- src/Commands/ListSubscriptionPlansCommand.php
+++ src/Commands/ListSubscriptionPlansCommand.php
@@ -1,33 +1,43 @@
 <?php
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace WMDE\Fundraising\PaymentContext\Commands;
 
+use GuzzleHttp\Client;
+use Psr\Log\NullLogger;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Dotenv\Dotenv;
+use WMDE\Fundraising\PaymentContext\Services\PayPal\GuzzlePaypalAPI;
 
 #[AsCommand(
 	name: 'app:list-subscription-plans',
 	description: 'Lists existing PayPal subscription plans.',
 	hidden: false,
 )]
-class ListSubscriptionPlansCommand extends Command
-{
+class ListSubscriptionPlansCommand extends Command {
 
-	protected function execute(InputInterface $input, OutputInterface $output): int {
-
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		// initialize API with environment parameters (Paypal url, client id and secret)
+		$api = new GuzzlePaypalAPI(
+			new Client( [ 'base_uri' => $_ENV['PAYPAL_URL'] ] ),
+			$_ENV['PAYPAL_API_CLIENT_ID'],
+			$_ENV['PAYPAL_API_CLIENT_SECRET'],
+			new NullLogger()
+		);
+
 		// call listProducts on API
+		$products = $api->listProducts();
+
 		// output product name and id
+
+		foreach ( $products as $product ) {
+			print_r( $product );
+		}
+
 		// iterate over products, call getSubscriptionPlansForProduct on each product
 		// iterate over plans, output each plan id, name and monthly interval (slightly indented)
-
-		$url = getenv("PAYPAL_URL");
-		print_r($_ENV);
-		$output->writeln("Url is $url");
 
 		return Command::SUCCESS;
 	}
