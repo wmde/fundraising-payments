d0e25266bcc45fdb4a253d99bccf15a7292b106c
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-06-23 12:35:31 +0200

    Refactor PayPalAPIConfigFactory
    Extract method
    Fix tests and CI
    
    Co-Authored-By: Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
    Co-Authored-By: Sperling-0 <tanuja.doriya@wikimedia.de>

--- src/Domain/PaymentUrlGenerator/PayPalAPIConfigFactory.php
+++ src/Domain/PaymentUrlGenerator/PayPalAPIConfigFactory.php
@@ -13,17 +13,42 @@ class PayPalAPIConfigFactory {
 	 */
 	public static function createConfig( array $allConfigs, string $productKey, string $languageKey ): PayPalAPIConfig {
 		$subconfig = $allConfigs['paypal_api'];
-		$plans = [];
-		foreach ( $subconfig[$productKey][$languageKey]['plans'] as $something ) {
-			$intervalName = $something['interval'];
-			$interval = PaymentInterval::from( $intervalName );
-			$plans[$interval->name] = new SubscriptionPlan( $something['name'], $subconfig[$productKey][$languageKey]['product_id'], $interval, $something['id'] );
+		if ( !isset( $subconfig[$productKey] ) ) {
+			throw new \LogicException( "'$productKey' does not exist in PayPal API configuration. Please check your configuration file." );
 		}
+
+		if ( !isset( $subconfig[$productKey][$languageKey] ) ) {
+			throw new \LogicException( "'$languageKey' does not exist in PayPal API configuration for product '$productKey'. Please check your configuration file." );
+		}
+
+		$plans = self::createSubscriptionPlans(
+			$subconfig[$productKey][$languageKey]['plans'],
+			$subconfig[$productKey][$languageKey]['product_id']
+		);
 		return new PayPalAPIConfig(
 			$subconfig[$productKey][$languageKey]['product_name'],
 			$subconfig[$productKey]['return_url'],
 			$subconfig[$productKey]['cancel_url'],
 			$plans
 		);
+	}
+
+	/**
+	 * @param array{"interval":int, "name":string, "id":string}[] $subscriptionPlansConfig
+	 * @param string $productId
+	 * @return array<SubscriptionPlan>
+	 */
+	private static function createSubscriptionPlans( array $subscriptionPlansConfig, string $productId ): array {
+		$plans = [];
+		foreach ( $subscriptionPlansConfig as $subscriptionPlanValues ) {
+			$interval = PaymentInterval::from( $subscriptionPlanValues['interval'] );
+			$plans[$interval->name] = new SubscriptionPlan(
+				$subscriptionPlanValues['name'],
+				$productId,
+				$interval,
+				$subscriptionPlanValues['id']
+			);
+		}
+		return $plans;
 	}
 }
