725ad2b16807daaba1446361207f672e6ec57a39
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-08-01 10:54:24 +0200

    Improve validation of PPL API config
    Use Symfony TreeBuilder checks to check the structure of the
    PayPal API configuration.
    
    Add tests for missing and empty file.
    
    This is for https://phabricator.wikimedia.org/T329161

--- src/Services/PayPal/PayPalURLGeneratorConfigSchema.php
+++ src/Services/PayPal/PayPalURLGeneratorConfigSchema.php
@@ -5,24 +5,35 @@ namespace WMDE\Fundraising\PaymentContext\Services\PayPal;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use WMDE\Fundraising\PaymentContext\Domain\Model\PaymentInterval;
 
 class PayPalURLGeneratorConfigSchema implements ConfigurationInterface {
 	public function getConfigTreeBuilder(): TreeBuilder {
 		$treeBuilder = new TreeBuilder( 'paypal_api' );
 		$treeBuilder->getRootNode()
 			->arrayPrototype()
+				->requiresAtLeastOneElement()
 				->arrayPrototype()
 					->children()
-						->scalarNode( 'product_id' )->end()
-						->scalarNode( 'product_name' )->end()
-						->scalarNode( 'return_url' )->end()
-						->scalarNode( 'cancel_url' )->end()
+						->scalarNode( 'product_id' )->isRequired()->end()
+						->scalarNode( 'product_name' )->isRequired()->end()
+						->scalarNode( 'return_url' )->isRequired()->end()
+						->scalarNode( 'cancel_url' )->isRequired()->end()
 						->arrayNode( 'subscription_plans' )
+							->isRequired()
 							->arrayPrototype()
 								->children()
-									->scalarNode( 'id' )->end()
-									->scalarNode( 'name' )->end()
-									->scalarNode( 'interval' )->end()
+									->scalarNode( 'id' )->isRequired()->end()
+									->scalarNode( 'name' )->isRequired()->end()
+									->enumNode( 'interval' )
+										->isRequired()
+										->values( [
+											PaymentInterval::Monthly->name,
+											PaymentInterval::Quarterly->name,
+											PaymentInterval::HalfYearly->name,
+											PaymentInterval::Yearly->name
+										] )
+									->end()
 								->end()
 							->end()
 						->end()
