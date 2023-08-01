1c9986af941c3bab138b386a29fecc620dad6c15
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-08-01 10:54:24 +0200

    Improve validation of PPL API config
    Use Symfony TreeBuilder checks to check the structure of the
    PayPal API configuration.
    
    Add tests for missing and empty file.
    
    This is for https://phabricator.wikimedia.org/T329161

--- tests/Unit/Services/PayPal/PayPalURLGeneratorConfigReaderTest.php
+++ tests/Unit/Services/PayPal/PayPalURLGeneratorConfigReaderTest.php
@@ -4,6 +4,8 @@ declare( strict_types=1 );
 namespace WMDE\Fundraising\PaymentContext\Tests\Unit\Services\PayPal;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Yaml\Exception\ParseException;
 use WMDE\Fundraising\PaymentContext\Services\PayPal\PayPalURLGeneratorConfigReader;
 
 /**
@@ -18,12 +20,36 @@ class PayPalURLGeneratorConfigReaderTest extends TestCase {
 	public function testProductIdsMustBeUnique(): void {
 		$this->expectException( \DomainException::class );
 		$this->expectExceptionMessage( "All product IDs in the configuration file must be unique!" );
-		$config = PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_duplicate_product_id.yml' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_duplicate_product_id.yml' );
 	}
 
 	public function testSubscriptionPlanIdsMustBeUnique(): void {
 		$this->expectException( \DomainException::class );
 		$this->expectExceptionMessage( "All subscription plan IDs in the configuration file must be unique!" );
-		$config = PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_duplicate_plan_id.yml' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_duplicate_plan_id.yml' );
+	}
+
+	public function testFileMustContainAtLeastOneLanguage(): void {
+		$this->expectException( InvalidConfigurationException::class );
+		$this->expectExceptionMessage( 'The path "paypal_api.donation" should have at least 1 element(s) defined.' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_no_language.yml' );
+	}
+
+	public function testFileMustContainAtLeastOnePlan(): void {
+		$this->expectException( InvalidConfigurationException::class );
+		$this->expectExceptionMessage( 'The child config "product_id" under "paypal_api.donation.de_DE" must be configured.' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_no_plans.yml' );
+	}
+
+	public function testReadingFromEmptyFileThrowsException(): void {
+		$this->expectException( \DomainException::class );
+		$this->expectExceptionMessage( 'Configuration file must contain a nested array structure!' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/../../../Data/PayPalAPIURLGeneratorConfig/paypal_api_empty.yml' );
+	}
+
+	public function testReadingFromNonexistentFileThrowsException(): void {
+		$this->expectException( ParseException::class );
+		$this->expectExceptionMessageMatches( '#does/not/exist/plan.yml" does not exist#' );
+		PayPalURLGeneratorConfigReader::readConfig( __DIR__ . '/this/path/does/not/exist/plan.yml' );
 	}
 }
