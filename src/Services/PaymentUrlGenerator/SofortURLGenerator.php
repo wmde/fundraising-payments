wmde/fundraising-payments

commit 744a69815dbc5b017ebeff6e2a38774dff475954

Allow authentication parameter passing
Remove userAccessToken and systemAccessToken from RequestContext.
Instead, allow the calling code to add authentication tokens. This
will allow us to change the URL parameters of our own application more
easily in the future.

https://phabricator.wikimedia.org/T344271

src/Services/PaymentUrlGenerator/SofortURLGenerator.php
@@ -10,22 +10,18 @@ use WMDE\Fundraising\PaymentContext\Domain\UrlGenerator\PaymentProviderURLGenera
 use WMDE\Fundraising\PaymentContext\Domain\UrlGenerator\RequestContext;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\Sofort\Request;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\Sofort\SofortClient;
+use WMDE\Fundraising\PaymentContext\Services\URLAuthenticator;
 
 class SofortURLGenerator implements PaymentProviderURLGenerator {
 
 	private const CURRENCY = 'EUR';
 
-	private SofortURLGeneratorConfig $config;
-	private SofortClient $client;
-	private SofortPayment $payment;
-
 	public function __construct(
-		SofortURLGeneratorConfig $config,
-		SofortClient $client,
-		SofortPayment $payment ) {
-		$this->config = $config;
-		$this->client = $client;
-		$this->payment = $payment;
+		private readonly SofortURLGeneratorConfig $config,
+		private readonly SofortClient $client,
+		private readonly URLAuthenticator $authenticator,
+		private readonly SofortPayment $payment
+	) {
 	}
 
 	public function generateUrl( RequestContext $requestContext ): string {
@@ -40,21 +36,11 @@ class SofortURLGenerator implements PaymentProviderURLGenerator {
 			$this->payment->getPaymentReferenceCode()
 		] );
 		$request->setSuccessUrl(
-			$this->config->getReturnUrl() . '?' . http_build_query(
-				[
-					'id' => $requestContext->itemId,
-					'accessToken' => $requestContext->accessToken
-				]
-			)
+			$this->authenticator->addAuthenticationTokensToApplicationUrl( $this->config->getReturnUrl() )
 		);
 		$request->setAbortUrl( $this->config->getCancelUrl() );
 		$request->setNotificationUrl(
-			$this->config->getNotificationUrl() . '?' . http_build_query(
-				[
-					'id' => $requestContext->itemId,
-					'updateToken' => $requestContext->updateToken
-				]
-			)
+			$this->authenticator->addAuthenticationTokensToApplicationUrl( $this->config->getNotificationUrl() )
 		);
 		$request->setLocale( $this->config->getLocale() );
 

tests/Fixtures/FakeUrlAuthenticator.php
@@ -1,0 +1,46 @@
+<?php
+declare( strict_types=1 );
+
+namespace WMDE\Fundraising\PaymentContext\Tests\Fixtures;
+
+use WMDE\Fundraising\PaymentContext\Services\URLAuthenticator;
+
+class FakeUrlAuthenticator implements URLAuthenticator {
+	public const TOKEN_PARAM = 'testAccessToken=LET_ME_IN';
+
+	public function addAuthenticationTokensToApplicationUrl( string $url ): string {
+		$urlParts = parse_url( $url );
+		if ( !is_array( $urlParts ) ) {
+			return $url;
+		}
+		$query = $urlParts['query'] ?? '';
+		$urlParts['query'] = $query ? "$query&" . self::TOKEN_PARAM : self::TOKEN_PARAM;
+		return $this->buildUrl( $urlParts );
+	}
+
+	/**
+	 * @param array<string,int|string> $urlParts
+	 * @return string
+	 */
+	private function buildUrl( array $urlParts ): string {
+		$scheme = isset( $urlParts['scheme'] ) ? $urlParts['scheme'] . '://' : '';
+
+		$host = $urlParts['host'] ?? '';
+
+		$path = $urlParts['path'] ?? '';
+
+		$query = isset( $urlParts['query'] ) ? '?' . $urlParts['query'] : '';
+
+		$fragment = isset( $urlParts['fragment'] ) ? '#' . $urlParts['fragment'] : '';
+
+		return "$scheme$host$path$query$fragment";
+	}
+
+	public function getAuthenticationTokensForPaymentProviderUrl( string $urlGeneratorClass, array $requestedParameters ): array {
+		$resultParameters = [];
+		foreach ( $requestedParameters as $idx => $parameter ) {
+			$resultParameters[$parameter] = 'p-test-token-' . $idx;
+		}
+		return $resultParameters;
+	}
+}

tests/Unit/Services/PaymentUrlGenerator/CreditCardURLGeneratorTest.php
@@ -12,6 +12,7 @@ use WMDE\Fundraising\PaymentContext\Domain\UrlGenerator\RequestContext;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\CreditCardURLGenerator;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\CreditCardURLGeneratorConfig;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\TranslatableDescription;
+use WMDE\Fundraising\PaymentContext\Tests\Fixtures\FakeUrlAuthenticator;
 
 /**
  * @covers \WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\CreditCardURLGenerator
@@ -47,13 +48,12 @@ class CreditCardURLGeneratorTest extends TestCase {
 				],
 				$translatableDescriptionMock
 			),
+			new FakeUrlAuthenticator(),
 			new CreditCardPayment( 42, $amount, $interval )
 		);
 
 		$requestContext = new RequestContext(
 			itemId: $donationId,
-			updateToken: $updateToken,
-			accessToken: $accessToken,
 			firstName: $firstName,
 			lastName: $lastName,
 		);
@@ -79,20 +79,19 @@ class CreditCardURLGeneratorTest extends TestCase {
 				],
 				$translatableDescriptionMock
 			),
+			new FakeUrlAuthenticator(),
 			new CreditCardPayment( 32, Euro::newFromCents( 100 ), PaymentInterval::OneTime )
 		);
 
 		$requestContext = new RequestContext(
 			itemId: 1234567,
-			updateToken: "my_update_token",
-			accessToken: "my_access_token",
 			firstName: "Kai",
 			lastName: "Nissen",
 		);
 		$this->assertSame(
 			'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 			'paytext=Ich+spende+einmalig&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-			'token=my_access_token&utoken=my_update_token&amount=100&theme=wikimedia&producttype=fee&lang=de&testmode=1',
+			'amount=100&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1&testmode=1',
 			$urlGenerator->generateUrl( $requestContext )
 		);
 	}
@@ -105,7 +104,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+einmalig&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-				'token=my_access_token&utoken=my_update_token&amount=500&theme=wikimedia&producttype=fee&lang=de',
+				'amount=500&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
 				'Kai',
 				'Nissen',
 				'Ich spende einmalig',
@@ -119,7 +118,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+monatlich&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-				'token=my_access_token&utoken=my_update_token&amount=123&theme=wikimedia&producttype=fee&lang=de',
+				'amount=123&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
 				'Kai',
 				'Nissen',
 				'Ich spende monatlich',
@@ -133,8 +132,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+halbj%C3%A4hrlich&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&' .
-				'gfx=wikimedia_black&token=my_access_token&utoken=my_update_token&amount=1250&theme=wikimedia&' .
-				'producttype=fee&lang=de',
+				'gfx=wikimedia_black&amount=1250&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
 				'Kai',
 				'Nissen',
 				'Ich spende halbjährlich',

tests/Unit/Services/PaymentUrlGenerator/LegacyPayPalURLGeneratorTest.php
@@ -12,6 +12,7 @@ use WMDE\Fundraising\PaymentContext\Domain\UrlGenerator\RequestContext;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\LegacyPayPalURLGenerator;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\LegacyPayPalURLGeneratorConfig;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\TranslatableDescription;
+use WMDE\Fundraising\PaymentContext\Tests\Fixtures\FakeUrlAuthenticator;
 
 /**
  * @covers \WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\LegacyPayPalURLGenerator
@@ -43,7 +44,7 @@ class LegacyPayPalURLGeneratorTest extends TestCase {
 			Euro::newFromString( '12.34' ),
 			PaymentInterval::Quarterly
 		);
-		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfig(), $payment );
+		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfig(), new FakeUrlAuthenticator(), $payment );
 
 		$this->assertUrlValidForSubscriptions(
 			$generator->generateUrl( $this->testRequestContext )
@@ -61,7 +62,7 @@ class LegacyPayPalURLGeneratorTest extends TestCase {
 			Euro::newFromString( '12.34' ),
 			PaymentInterval::OneTime
 		);
-		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfig(), $payment );
+		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfig(), new FakeUrlAuthenticator(), $payment );
 
 		$this->assertUrlValidForSinglePayments(
 			$generator->generateUrl( $this->testRequestContext )
@@ -116,7 +117,7 @@ class LegacyPayPalURLGeneratorTest extends TestCase {
 			PaymentInterval::Quarterly
 		);
 
-		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfigWithDelayedPayment(), $payment );
+		$generator = new LegacyPayPalURLGenerator( $this->newPayPalUrlConfigWithDelayedPayment(), new FakeUrlAuthenticator(), $payment );
 
 		$this->assertUrlValidForDelayedSubscriptions(
 			$generator->generateUrl( $this->testRequestContext )
@@ -157,10 +158,10 @@ class LegacyPayPalURLGeneratorTest extends TestCase {
 		$this->assertStringContainsString( 'notify_url=https%3A%2F%2Fmy.donation.app%2Fhandler%2Fpaypal%2F', $generatedUrl );
 		$this->assertStringContainsString( 'cancel_return=https%3A%2F%2Fmy.donation.app%2Fdonation%2Fcancel%2F', $generatedUrl );
 		$this->assertStringContainsString(
-			'return=https%3A%2F%2Fmy.donation.app%2Fdonation%2Fconfirm%2F%3Fid%3D1234%26accessToken%3Datoken',
+			'return=https%3A%2F%2Fmy.donation.app%2Fdonation%2Fconfirm%2F%3Fid%3D1234%26testAccessToken%3DLET_ME_IN',
 			$generatedUrl
 		);
-		$this->assertStringContainsString( 'custom=%7B%22sid%22%3A1234%2C%22utoken%22%3A%22utoken%22%7D', $generatedUrl );
+		$this->assertStringContainsString( 'custom=p-test-token-0', $generatedUrl );
 	}
 
 	private function assertSinglePaymentRelatedParamsSet( string $generatedUrl ): void {

tests/Unit/Services/PaymentUrlGenerator/SofortURLGeneratorTest.php
@@ -11,10 +11,11 @@ use WMDE\Fundraising\PaymentContext\Domain\Model\PaymentInterval;
 use WMDE\Fundraising\PaymentContext\Domain\Model\PaymentReferenceCode;
 use WMDE\Fundraising\PaymentContext\Domain\Model\SofortPayment;
 use WMDE\Fundraising\PaymentContext\Domain\UrlGenerator\RequestContext;
-use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\SofortURLGenerator as SofortUrlGenerator;
-use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\SofortURLGeneratorConfig as SofortUrlConfig;
+use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\SofortURLGenerator;
+use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\SofortURLGeneratorConfig;
 use WMDE\Fundraising\PaymentContext\Services\PaymentUrlGenerator\TranslatableDescription;
 use WMDE\Fundraising\PaymentContext\Tests\Fixtures\ExceptionThrowingSofortSofortClient;
+use WMDE\Fundraising\PaymentContext\Tests\Fixtures\FakeUrlAuthenticator;
 use WMDE\Fundraising\PaymentContext\Tests\Fixtures\SofortSofortClientSpy;
 
 /**
@@ -26,12 +27,10 @@ class SofortURLGeneratorTest extends TestCase {
 		$internalItemId = 44;
 		$externalItemId = 'wx529836';
 		$amount = Euro::newFromCents( 600 );
-		$updateToken = 'UDtoken';
-		$accessToken = 'XStoken';
 		$locale = 'DE';
 		$translatableDescription = $this->createMock( TranslatableDescription::class );
 
-		$config = new SofortUrlConfig(
+		$config = new SofortURLGeneratorConfig(
 			$locale,
 			'https://us.org/yes',
 			'https://us.org/no',
@@ -45,20 +44,16 @@ class SofortURLGeneratorTest extends TestCase {
 			PaymentInterval::OneTime,
 			$this->createMock( PaymentReferenceCode::class ) );
 
-		$urlGenerator = new SofortUrlGenerator( $config, $client, $payment );
+		$urlGenerator = new SofortURLGenerator( $config, $client, new FakeUrlAuthenticator(), $payment );
 
 		$requestContext = new RequestContext(
 			$internalItemId,
-			$externalItemId,
-			$updateToken,
-			$accessToken
+			$externalItemId
 		);
 		$urlGenerator->generateUrl( $requestContext );
 
-		$this->assertStringContainsString( "id=$internalItemId", $client->request->getSuccessUrl() );
-		$this->assertStringContainsString( "id=$internalItemId", $client->request->getNotificationUrl() );
-		$this->assertStringContainsString( "accessToken=$accessToken", $client->request->getSuccessUrl() );
-		$this->assertStringContainsString( "updateToken=$updateToken", $client->request->getNotificationUrl() );
+		$this->assertStringContainsString( "testAccessToken=LET_ME_IN", $client->request->getSuccessUrl() );
+		$this->assertStringContainsString( "testAccessToken=LET_ME_IN", $client->request->getNotificationUrl() );
 		$this->assertSame( $amount, $client->request->getAmount() );
 		$this->assertSame( $locale, $client->request->getLocale() );
 	}
@@ -66,7 +61,7 @@ class SofortURLGeneratorTest extends TestCase {
 	public function testSofortUrlGeneratorReturnsUrlFromClient(): void {
 		$expectedUrl = 'https://dn.ht/picklecat/';
 		$translatableDescriptionMock = $this->createMock( TranslatableDescription::class );
-		$config = new SofortUrlConfig(
+		$config = new SofortURLGeneratorConfig(
 			'DE',
 			'https://us.org/yes',
 			'https://us.org/no',
@@ -81,13 +76,12 @@ class SofortURLGeneratorTest extends TestCase {
 			PaymentInterval::OneTime,
 			$this->createMock( PaymentReferenceCode::class ) );
 
-		$urlGenerator = new SofortUrlGenerator( $config, $client, $payment );
+		$urlGenerator = new SofortURLGenerator( $config, $client, new FakeUrlAuthenticator(), $payment );
 
 		$requestContext = new RequestContext(
 			44,
 			'wx529836',
-			'up date token :)',
-			'ax ess token :)' );
+			);
 		$returnedUrl = $urlGenerator->generateUrl( $requestContext );
 
 		$this->assertSame( $expectedUrl, $returnedUrl );
@@ -95,7 +89,7 @@ class SofortURLGeneratorTest extends TestCase {
 
 	public function testWhenApiReturnsErrorAnExceptionWithApiErrorMessageIsThrown(): void {
 		$translatableDescriptionStub = $this->createStub( TranslatableDescription::class );
-		$config = new SofortUrlConfig(
+		$config = new SofortURLGeneratorConfig(
 			'DE',
 			'https://irreleva.nt/y',
 			'https://irreleva.nt/n',
@@ -107,19 +101,15 @@ class SofortURLGeneratorTest extends TestCase {
 			23,
 			Euro::newFromCents( 600 ),
 			PaymentInterval::OneTime,
-			$this->createMock( PaymentReferenceCode::class ) );
+			$this->createMock( PaymentReferenceCode::class )
+		);
 
-		$urlGenerator = new SofortUrlGenerator( $config, $client, $payment );
+		$urlGenerator = new SofortURLGenerator( $config, $client, new FakeUrlAuthenticator(), $payment );
 
 		$this->expectException( RuntimeException::class );
 		$this->expectExceptionMessage( 'Could not generate Sofort URL: boo boo' );
 
-		$requestContext = new RequestContext(
-			itemId: 23,
-			updateToken: 'token_to_updateblabla',
-			accessToken: 'token_to_accessblabla'
-
-		);
+		$requestContext = new RequestContext( itemId: 23 );
 		$urlGenerator->generateUrl( $requestContext );
 	}
 }

config/paypal_api.example.yml
@@ -38,12 +38,12 @@ donation:
               interval: 'Yearly'
         # the page the user gets redirected to when the PayPal payment was successfully created
         # Where do they get redirected from? From PayPal page
-        # PayPalAPIURLGenerator replaces the placeholders before the URL gets transmitted to PayPal
-        return_url: 'https://test-spenden.wikimedia.de/show-donation-confirmation?id={{id}}&utoken={{updateToken}}&token={{accessToken}}'
+        # The UrlAuthenticator will append access tokens to the URL
+        return_url: 'https://test-spenden.wikimedia.de/show-donation-confirmation?'
         # the page the user gets redirected to when they hit 'cancel'
         # Where do they get redirected from? From PayPal page
-        # PayPalAPIURLGenerator replaces the placeholders before the URL gets transmitted to PayPal
-        cancel_url: 'https://test-spenden.wikimedia.de/new?id={{id}}&utoken={{updateToken}}&token={{accessToken}}'
+      # The UrlAuthenticator will append access tokens to the URL
+        cancel_url: 'https://test-spenden.wikimedia.de/new?id='
 
     en_GB:
         product_id: 'donation_en_1'
@@ -61,8 +61,8 @@ donation:
             - id: 'NMP'
               name: 'Yearly donation to Wikimedia'
               interval: 'Yearly'
-        return_url: 'https://test-spenden.wikimedia.de/show-donation-confirmation?id={{id}}&utoken={{updateToken}}&token={{accessToken}}&lang=en'
-        cancel_url: 'https://test-spenden.wikimedia.de/new?id={{id}}&utoken={{updateToken}}&token={{accessToken}}&lang=en'
+        return_url: 'https://test-spenden.wikimedia.de/show-donation-confirmation?&lang=en'
+        cancel_url: 'https://test-spenden.wikimedia.de/new?&lang=en'
 membership:
     de_DE:
         product_id: 'membership_de_1'
@@ -80,8 +80,8 @@ membership:
             - id: 'MBB'
               name: 'Jährlicher Mitgliedschaftsbeitrag an Wikimedia'
               interval: 'Yearly'
-        return_url: 'https://test-spenden.wikimedia.de/show-membership-confirmation?id={{id}}&utoken={{updateToken}}&token={{accessToken}}'
-        cancel_url: 'https://test-spenden.wikimedia.de/apply-for-membership?id={{id}}&utoken={{updateToken}}&token={{accessToken}}'
+        return_url: 'https://test-spenden.wikimedia.de/show-membership-confirmation?'
+        cancel_url: 'https://test-spenden.wikimedia.de/apply-for-membership?'
     en_GB:
         product_id: 'membership_en_1'
         product_name: 'Wikimedia Membership'
@@ -98,5 +98,5 @@ membership:
             - id: 'MNB'
               name: 'Yearly membership fee for Wikimedia'
               interval: 'Yearly'
-        return_url: 'https://test-spenden.wikimedia.de/show-membership-confirmation?id={{id}}&utoken={{updateToken}}&token={{accessToken}}&lang=en'
-        cancel_url: 'https://test-spenden.wikimedia.de/apply-for-membership?id={{id}}&utoken={{updateToken}}&token={{accessToken}}&lang=en'
+        return_url: 'https://test-spenden.wikimedia.de/show-membership-confirmation?lang=en'
+        cancel_url: 'https://test-spenden.wikimedia.de/apply-for-membership?lang=en'

src/Domain/UrlGenerator/RequestContext.php
@@ -9,17 +9,12 @@ class RequestContext {
 	/**
 	 * @param int $itemId (internal) donation ID or membership ID
 	 * @param string $invoiceId unique, currently derived from donation/membership ID
-	 * @param string $updateToken A token to use to invoke our API to change payment details at a later point in time
-	 * @param string $accessToken A token to use to return to the payment process after completing the 3rd party process
 	 * @param string $firstName
 	 * @param string $lastName
 	 */
 	public function __construct(
 		public readonly int $itemId,
 		public readonly string $invoiceId = '',
-		public readonly string $updateToken = '',
-		public readonly string $accessToken = '',
-
 		public readonly string $firstName = '',
 		public readonly string $lastName = ''
 	) {

src/Services/PayPal/DomainUrlTemplate.php
@@ -1,37 +1,0 @@
-<?php
-declare( strict_types=1 );
-
-namespace WMDE\Fundraising\PaymentContext\Services\PayPal;
-
-use WMDE\Fundraising\PaymentContext\UseCases\CreatePayment\DomainSpecificContext;
-
-class DomainUrlTemplate {
-	/**
-	 * @var array<string,string>
-	 */
-	private array $context;
-
-	public function __construct( DomainSpecificContext $context ) {
-		$this->context = [
-			'id' => (string)$context->itemId,
-			'userAccessToken' => $context->userAccessToken,
-			'systemAccessToken' => $context->systemAccessToken,
-		];
-
-		// Only needed until we actually use the user and system access tokens, see https://phabricator.wikimedia.org/T344346
-		if ( str_contains( $context->userAccessToken, ':' ) ) {
-			$this->context = array_merge(
-				$context->getLegacyTokens(),
-				$this->context
-			);
-		}
-	}
-
-	public function replacePlaceholders( string $url ): string {
-		return str_replace(
-			array_map( fn( $key ) => '{{' . $key . '}}', array_keys( $this->context ) ),
-			array_values( $this->context ),
-			$url
-		);
-	}
-}

commit 3e67d8a3a4bf1a2016c2f3f27caaf0ba2bd1a2a0

Fix SofortURLGenerator
Commit 744a69815dbc5b017ebeff6e2a38774dff475954 introduced an error
where the `SofortURLGenerator` appended the wrong parameters to the
notification URL (`accessToken` instead of `updateToken`). This commit
fixes SofortURLGenerator.

This commit also changes the generated value in FakeUrlAuthenticator
because it generates *parameters* and not only *tokens*. This results in
changes for all URL Generator classes.

The API stays the same, but the Frontend will throw a DomainException
when this is merged. But since Major Releases of bounded context are
such hassle, I decided to declare this a 7.x compatible

This fixes https://phabricator.wikimedia.org/T350149

src/Services/PaymentUrlGenerator/SofortURLGenerator.php
@@ -40,7 +40,12 @@ class SofortURLGenerator implements PaymentCompletionURLGenerator {
 		);
 		$request->setAbortUrl( $this->config->getCancelUrl() );
 		$request->setNotificationUrl(
-			$this->authenticator->addAuthenticationTokensToApplicationUrl( $this->config->getNotificationUrl() )
+			$this->config->getNotificationUrl() . '?' . http_build_query(
+				$this->authenticator->getAuthenticationTokensForPaymentProviderUrl(
+					self::class,
+					[ 'id', 'updateToken' ]
+				)
+			)
 		);
 		$request->setLocale( $this->config->getLocale() );
 

tests/Fixtures/FakeUrlAuthenticator.php
@@ -7,6 +7,7 @@ use WMDE\Fundraising\PaymentContext\Services\URLAuthenticator;
 
 class FakeUrlAuthenticator implements URLAuthenticator {
 	public const TOKEN_PARAM = 'testAccessToken=LET_ME_IN';
+	public const PAYMENT_PROVIDER_PARAM_PREFIX = 'p-test-param-';
 
 	public function addAuthenticationTokensToApplicationUrl( string $url ): string {
 		$urlParts = parse_url( $url );
@@ -39,7 +40,7 @@ class FakeUrlAuthenticator implements URLAuthenticator {
 	public function getAuthenticationTokensForPaymentProviderUrl( string $urlGeneratorClass, array $requestedParameters ): array {
 		$resultParameters = [];
 		foreach ( $requestedParameters as $idx => $parameter ) {
-			$resultParameters[$parameter] = 'p-test-token-' . $idx;
+			$resultParameters[$parameter] = self::PAYMENT_PROVIDER_PARAM_PREFIX . $idx;
 		}
 		return $resultParameters;
 	}

tests/Unit/Services/PaymentUrlGenerator/CreditCardURLGeneratorTest.php
@@ -91,7 +91,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 		$this->assertSame(
 			'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 			'paytext=Ich+spende+einmalig&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-			'amount=100&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1&testmode=1',
+			'amount=100&theme=wikimedia&producttype=fee&lang=de&token=p-test-param-0&utoken=p-test-param-1&testmode=1',
 			$urlGenerator->generateUrl( $requestContext )
 		);
 	}
@@ -104,7 +104,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+einmalig&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-				'amount=500&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
+				'amount=500&theme=wikimedia&producttype=fee&lang=de&token=p-test-param-0&utoken=p-test-param-1',
 				'Kai',
 				'Nissen',
 				'Ich spende einmalig',
@@ -118,7 +118,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+monatlich&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&gfx=wikimedia_black&' .
-				'amount=123&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
+				'amount=123&theme=wikimedia&producttype=fee&lang=de&token=p-test-param-0&utoken=p-test-param-1',
 				'Kai',
 				'Nissen',
 				'Ich spende monatlich',
@@ -132,7 +132,7 @@ class CreditCardURLGeneratorTest extends TestCase {
 			[
 				'https://credit-card.micropayment.de/creditcard/event/index.php?project=wikimedia&bgcolor=CCE7CD&' .
 				'paytext=Ich+spende+halbj%C3%A4hrlich&mp_user_firstname=Kai&mp_user_surname=Nissen&sid=1234567&' .
-				'gfx=wikimedia_black&amount=1250&theme=wikimedia&producttype=fee&lang=de&token=p-test-token-0&utoken=p-test-token-1',
+				'gfx=wikimedia_black&amount=1250&theme=wikimedia&producttype=fee&lang=de&token=p-test-param-0&utoken=p-test-param-1',
 				'Kai',
 				'Nissen',
 				'Ich spende halbjährlich',

tests/Unit/Services/PaymentUrlGenerator/LegacyPayPalURLGeneratorTest.php
@@ -150,7 +150,7 @@ class LegacyPayPalURLGeneratorTest extends TestCase {
 			'return=https%3A%2F%2Fmy.donation.app%2Fdonation%2Fconfirm%2F%3Fid%3D1234%26testAccessToken%3DLET_ME_IN',
 			$generatedUrl
 		);
-		$this->assertStringContainsString( 'custom=p-test-token-0', $generatedUrl );
+		$this->assertStringContainsString( 'custom=p-test-param-0', $generatedUrl );
 	}
 
 	private function assertSinglePaymentRelatedParamsSet( string $generatedUrl ): void {

tests/Unit/Services/PaymentUrlGenerator/SofortURLGeneratorTest.php
@@ -54,7 +54,9 @@ class SofortURLGeneratorTest extends TestCase {
 		$urlGenerator->generateUrl( $requestContext );
 
 		$this->assertStringContainsString( "testAccessToken=LET_ME_IN", $client->request->getSuccessUrl() );
-		$this->assertStringContainsString( "testAccessToken=LET_ME_IN", $client->request->getNotificationUrl() );
+		$notificationUrl = $client->request->getNotificationUrl();
+		$this->assertStringContainsString( "id=p-test-param-", $notificationUrl );
+		$this->assertStringContainsString( "updateToken=p-test-param-", $notificationUrl );
 		$this->assertSame( $amount, $client->request->getAmount() );
 		$this->assertSame( $locale, $client->request->getLocale() );
 	}